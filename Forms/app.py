9f4765c72aff7aec807f380e1fa8525045902898
Khyber Sen <kkysen@gmail.com>
2017-10-02 13:19:23 -0400

    finished Echo HW

--- Forms/app.py
+++ Forms/app.py
@@ -5,7 +5,8 @@ from flask import Response
 from flask import render_template
 from flask import request
 
-from util.flask_utils import redirect_url
+# noinspection PyUnresolvedReferences
+import util.flask_utils
 
 app = Flask(__name__)
 
@@ -14,17 +15,22 @@ app = Flask(__name__)
 @app.route('/login')
 def login():
     # type: () -> str
-    return render_template('form.jinja2')
+    return render_template('login.jinja2')
 
 
 @app.route('/auth', methods=['post'])
 def authorize():
     # type: () -> Response
     form = request.form
+    print(form)
     username = form['username']
     password = form['password']
     print(username, password)
-    return redirect_url(login)
+    return render_template(
+        'after_login.jinja2',
+        username=username,
+        password=password,
+        method=request.method)
 
 
 if __name__ == '__main__':
